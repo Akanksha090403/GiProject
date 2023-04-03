937b6151a461d3780de7af6716c3e9f431b95d39
Akanksha Singh <Akanksha Singh@LAPTOP-P6OSDITE>
2023-04-03 15:59:25 +0530

    Ist repository

---
+++ src/Test1.java
@@ -0,0 +1,16 @@
+public class Test1 {
+   public static void main(String ar[])
+   {
+	   String st1="Yes";
+	   if(st1.equals("Yes"))
+	   {
+		   System.out.println("ok...do something");
+		   
+	   }
+	   else
+	   {
+		   System.out.println("do nothing");  
+	   }
+			   
+   }
+}
